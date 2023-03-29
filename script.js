3d349720820da8d4b69bac9f50c6f2fcd6af75a7
Bradley Rispone <bradrispone@gmail.com>
2023-03-29 15:15:24 -0500

    Refactor card creation to account for read/unread status

--- script.js
+++ script.js
@@ -30,23 +30,33 @@ function populateLibrary() {
         const newBookDiv = document.createElement("div");
         
         newBookDiv.className = "card";
+        newBookDiv.dataset.index = index;
 
         newBookDiv.innerHTML = `<img class='cover-img' src='${book.cover ? book.cover : './images/book-cover-placeholder.png'}'/>` +
                                `"${book.title}"<br>` +
                                `by<br>` +
-                               `${book.author}<br>` +
-                               `${book.pages} pages<br>` +
-                               `Status: ${book.status}`;
+                               `${book.author}`;
 
         const deleteButton = document.createElement("img");
         deleteButton.className = 'delete-btn';
         deleteButton.src = './icons/trashcan.svg';
-        deleteButton.dataset.index = index;
         deleteButton.addEventListener('click', (event) => {
-            removeFromLibrary(event.target.dataset.index);
+            removeFromLibrary(event.target.parentNode.dataset.index);
         });
 
         newBookDiv.prepend(deleteButton);
+
+        //create foooter
+        const cardFooter = document.createElement("div");
+        cardFooter.className = "card-footer";
+        cardFooter.innerHTML = `<span class="pg-count">${book.pages} pages</span>`;
+        //create read/unread button w/ event listener
+        const statusIcon = document.createElement("img");
+        statusIcon.src = book.status ? './icons/read.svg' : './icons/unread.svg';
+        //append button to footer
+        cardFooter.append(statusIcon);
+        //append footer to card
+        newBookDiv.append(cardFooter);
 
         bookshelf.append(newBookDiv);
     });
@@ -78,7 +88,7 @@ document.getElementById("new-book-form").addEventListener("submit", (event) => {
     const author = document.getElementById("book-author-input").value;
     const cover = document.getElementById("book-cover-input").value;
     const pages = document.getElementById("book-pages-input").value;
-    const status = document.getElementById("book-status-input").value;
+    const status = (document.getElementById("book-status-input").value === "true"); // will set it to true if the string is 'true', otherwise false
 
     addBookToLibrary(title, author, cover, pages, status);
     document.getElementById("new-book-modal").style.display = "none"; // Hide modal
